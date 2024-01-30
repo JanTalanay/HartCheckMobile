6b25cfe6312f53d5ea46fcbe69c3859ce8dc18e3
Jems <jamescarlo.maranion@benile.edu.ph>
2024-01-30 21:31:57 +0800

    Fixed modals for -> register_prev_med = prev med condition                  -> register_prevmedicine = prev medicine                  -> register_surg_history = prev surg

--- app/src/main/java/com/example/hartcheck/RegisterPreviousMed.kt
+++ app/src/main/java/com/example/hartcheck/RegisterPreviousMed.kt
@@ -1,15 +1,21 @@
 package com.example.hartcheck
 
 import android.app.DatePickerDialog
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
+import android.view.Window
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import com.example.hartcheck.Model.MedicalHistory
 import com.example.hartcheck.Remote.MedHisRemote.MedHisInstance
@@ -23,7 +29,6 @@ import java.util.Locale
 
 class RegisterPreviousMed : AppCompatActivity() {
 
-    private lateinit var tvDatePicker: TextView
     private val calendar = Calendar.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,57 +40,11 @@ class RegisterPreviousMed : AppCompatActivity() {
         val patientID = intent.getIntExtra("patientID", 0)
         Toast.makeText(this@RegisterPreviousMed, "$patientID", Toast.LENGTH_SHORT).show()
 
-        tvDatePicker = findViewById(R.id.txt_medcond_dates)
-
-
-        tvDatePicker.setOnClickListener {
-            // Show the DatePicker dialog
-            showDatePicker()
-        }
-
         val addButton = findViewById<Button>(R.id.btn_addMed)
         addButton.setOnClickListener {
             if (inputFields.childCount < 5 && inputFieldsDate.childCount <5){
-                val inputField = EditText(this)
-                val inputFieldDate = EditText(this)
-                inputField.hint = this.getString(R.string.input_heredetary)
-                inputFieldDate.hint = "Date"
-
-                inputField.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-                inputFieldDate.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-                inputFieldDate.setPadding(41,25,25,25)
-                inputFieldDate.isClickable = true
-                inputFieldDate.isFocusable = true
-                inputFieldDate.textSize = 18F
-                inputFieldDate.setOnClickListener {
-                    // Show the DatePicker dialog
-                    val datePickerDialog = DatePickerDialog(
-                        this, {DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
-                            val selectedDate = Calendar.getInstance()
-                            selectedDate.set(year, monthOfYear, dayOfMonth)
-                            val dateFormat = SimpleDateFormat("yyyy-dd-MM", Locale.getDefault())
-                            val formattedDate = dateFormat.format(selectedDate.time)
-                            inputFieldDate.hint = "$formattedDate"
-                        },
-                        calendar.get(Calendar.YEAR),
-                        calendar.get(Calendar.MONTH),
-                        calendar.get(Calendar.DAY_OF_MONTH)
-                    )
-                    // Show the DatePicker dialog
-                    datePickerDialog.show()
-                }
-
-
-                inputFields.addView(inputField)
-                inputFieldsDate.addView(inputFieldDate)
+                showModal()
             }
-
         }
 
         val removeButton = findViewById<Button>(R.id.btn_removeMed)
@@ -99,26 +58,6 @@ class RegisterPreviousMed : AppCompatActivity() {
         //this is where you get values
         val sendBtn = findViewById<Button>(R.id.btn_medHis_bodyMass)
         sendBtn.setOnClickListener {
-//            val values = mutableListOf<String>()
-//            val dates = mutableListOf<String>()
-//            for (i in 0 until inputFields.childCount) {
-//                val view: View = inputFields.getChildAt(i)
-//                if (view is EditText) {
-//                    values.add(view.text.toString())
-//                }
-//            }
-//            for (i in 0 until inputFieldsDate.childCount) {
-//                val view: View = inputFieldsDate.getChildAt(i)
-//                if (view is TextView) {
-//                    dates.add(view.text.toString())
-//                }
-//            }
-//            for (i in values){
-//                Log.d("Text", i)
-//            }
-//            for (i in dates){
-//                Log.d("Date", i)
-//            }
             val medHisInfos = mutableListOf<MedicalHistory>()
             for (i in 0 until inputFields.childCount) {
                 val view: View = inputFields.getChildAt(i)
@@ -135,26 +74,71 @@ class RegisterPreviousMed : AppCompatActivity() {
             insertMultipleMedHis(medHisInfos)
         }
 
-//        btnmedHisbodyMass.setOnClickListener{
-//            insertMedHisOld()
-//        }
     }
-    private fun showDatePicker() {
-        // Create a DatePickerDialog
-        val datePickerDialog = DatePickerDialog(
-            this, {DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
-                val selectedDate = Calendar.getInstance()
-                selectedDate.set(year, monthOfYear, dayOfMonth)
-                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-                val formattedDate = dateFormat.format(selectedDate.time)
-                tvDatePicker.text = "$formattedDate"
-            },
-            calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH)
-        )
-        // Show the DatePicker dialog
-        datePickerDialog.show()
+    private fun showModal(){
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.popup_register_prev_med)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val btn_close:Button = dialog.findViewById(R.id.btn_modal_deny_med)
+        val btn_confirm = dialog.findViewById<Button>(R.id.btn_modal_confirm_med)
+        val input_med = dialog.findViewById<EditText>(R.id.edit_prev_med)
+        val input_date = dialog.findViewById<EditText>(R.id.edit_prev_med_date)
+
+        input_date.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(
+                this, {DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                    val selectedDate = Calendar.getInstance()
+                    selectedDate.set(year, monthOfYear, dayOfMonth)
+                    val dateFormat = SimpleDateFormat("yyyy-dd-MM", Locale.getDefault())
+                    val formattedDate = dateFormat.format(selectedDate.time)
+                    input_date.setText("$formattedDate")
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            )
+            // Show the DatePicker dialog
+            datePickerDialog.show()
+        }
+        btn_confirm.setOnClickListener {
+            val inputFields = findViewById<LinearLayout>(R.id.inputFields)
+            val inputFieldsDate = findViewById<LinearLayout>(R.id.inputFieldsDate)
+            val inputField = TextView(this)
+            val inputFieldDate = TextView(this)
+            inputField.text = input_med.text
+            inputFieldDate.text = input_date.text
+
+            inputField.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                gravity = Gravity.CENTER
+            }
+            inputFieldDate.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                gravity = Gravity.CENTER
+            }
+            inputField.setPadding(41,25,25,25)
+            inputFieldDate.setPadding(41,25,25,25)
+            inputField.textSize = 18F
+            inputFieldDate.textSize = 18F
+
+
+            inputFields.addView(inputField)
+            inputFieldsDate.addView(inputFieldDate)
+            dialog.dismiss()
+        }
+
+        btn_close.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 //    private fun insertMedHis(medHisInfos: List<MedicalHistory>) {
 //        val medHisService = MedHisInstance.retrofitBuilder
@@ -176,38 +160,38 @@ class RegisterPreviousMed : AppCompatActivity() {
 //            })
 //        }
 //    }
-    private fun insertMedHisOld() {//to be fix
-        val patientID = intent.getIntExtra("patientID", 0)
-
-        val txtMedCond: EditText = findViewById(R.id.txt_medcond)
-        val txtmedconddate: EditText = findViewById(R.id.txt_medcond_dates)
-
-        val medCond = txtMedCond.text.toString()
-        val date = txtmedconddate.text.toString()
-
-        val medHisInfo = MedicalHistory(patientID = patientID, medicalHistory = medCond, date = date)
-
-        val medHisService = MedHisInstance.retrofitBuilder
-        medHisService.insertMedHis(medHisInfo).enqueue(object : Callback<MedicalHistory> {
-            override fun onResponse(call: Call<MedicalHistory>, response: Response<MedicalHistory>) {
-                if (response.isSuccessful) {
-                    // Successfully deleted the bug report
-                    Toast.makeText(this@RegisterPreviousMed, "added the history", Toast.LENGTH_SHORT).show()
-
-                } else {
-                    val errorBody = response.errorBody()?.string()
-                    Log.d("MainActivity", "Failed to connect: " + response.code() + ", error: " + errorBody)
-
-                }
-            }
-
-            override fun onFailure(call: Call<MedicalHistory>, t: Throwable) {
-                // Handle network or other exceptions
-                Log.d("MainActivity", "Failed to connect: : " + t.message)
-
-            }
-        })
-    }
+//    private fun insertMedHisOld() {//to be fix
+//        val patientID = intent.getIntExtra("patientID", 0)
+//
+//        val txtMedCond: EditText = findViewById(R.id.txt_medcond)
+//        val txtmedconddate: EditText = findViewById(R.id.txt_medcond_dates)
+//
+//        val medCond = txtMedCond.text.toString()
+//        val date = txtmedconddate.text.toString()
+//
+//        val medHisInfo = MedicalHistory(patientID = patientID, medicalHistory = medCond, date = date)
+//
+//        val medHisService = MedHisInstance.retrofitBuilder
+//        medHisService.insertMedHis(medHisInfo).enqueue(object : Callback<MedicalHistory> {
+//            override fun onResponse(call: Call<MedicalHistory>, response: Response<MedicalHistory>) {
+//                if (response.isSuccessful) {
+//                    // Successfully deleted the bug report
+//                    Toast.makeText(this@RegisterPreviousMed, "added the history", Toast.LENGTH_SHORT).show()
+//
+//                } else {
+//                    val errorBody = response.errorBody()?.string()
+//                    Log.d("MainActivity", "Failed to connect: " + response.code() + ", error: " + errorBody)
+//
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<MedicalHistory>, t: Throwable) {
+//                // Handle network or other exceptions
+//                Log.d("MainActivity", "Failed to connect: : " + t.message)
+//
+//            }
+//        })
+//    }
     private fun insertMultipleMedHis(medHisInfos: List<MedicalHistory>) {
         val medHisService = MedHisInstance.retrofitBuilder
         medHisInfos.forEach { medHisInfo ->
--- app/src/main/java/com/example/hartcheck/RegisterSurgHistory.kt
+++ app/src/main/java/com/example/hartcheck/RegisterSurgHistory.kt
@@ -1,10 +1,15 @@
 package com.example.hartcheck
 
 import android.app.DatePickerDialog
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
+import android.view.Window
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
@@ -15,62 +20,17 @@ import java.util.Locale
 
 class RegisterSurgHistory : AppCompatActivity() {
 
-    private lateinit var tvDatePicker: TextView
     private val calendar = Calendar.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_surg_history)
         val inputFields = findViewById<LinearLayout>(R.id.inputFields3)
         val inputFieldsDate = findViewById<LinearLayout>(R.id.inputFieldsDate3)
-        tvDatePicker = findViewById(R.id.txt_surg_date)
-
-
-        tvDatePicker.setOnClickListener {
-            // Show the DatePicker dialog
-            showDatePicker()
-        }
 
         val addButton = findViewById<Button>(R.id.btn_addSurg)
         addButton.setOnClickListener {
             if (inputFields.childCount < 5 && inputFieldsDate.childCount <5){
-                val inputField = EditText(this)
-                val inputFieldDate = TextView(this)
-                inputField.hint = this.getString(R.string.input_surgery)
-                inputFieldDate.text = "Date"
-
-                inputField.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-                inputFieldDate.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-                inputFieldDate.setPadding(41,25,25,25)
-                inputFieldDate.isClickable = true
-                inputFieldDate.isFocusable = true
-                inputFieldDate.textSize = 18F
-                inputFieldDate.setOnClickListener {
-                    // Show the DatePicker dialog
-                    val datePickerDialog = DatePickerDialog(
-                        this, {DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
-                            val selectedDate = Calendar.getInstance()
-                            selectedDate.set(year, monthOfYear, dayOfMonth)
-                            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-                            val formattedDate = dateFormat.format(selectedDate.time)
-                            inputFieldDate.text = "$formattedDate"
-                        },
-                        calendar.get(Calendar.YEAR),
-                        calendar.get(Calendar.MONTH),
-                        calendar.get(Calendar.DAY_OF_MONTH)
-                    )
-                    // Show the DatePicker dialog
-                    datePickerDialog.show()
-                }
-
-
-                inputFields.addView(inputField)
-                inputFieldsDate.addView(inputFieldDate)
+                showModal()
             }
 
         }
@@ -88,41 +48,73 @@ class RegisterSurgHistory : AppCompatActivity() {
         sendBtn.setOnClickListener {
             val values = mutableListOf<String>()
             val dates = mutableListOf<String>()
-            for (i in 0 until inputFields.childCount) {
-                val view: View = inputFields.getChildAt(i)
-                if (view is EditText) {
-                    values.add(view.text.toString())
-                }
-            }
-            for (i in 0 until inputFieldsDate.childCount) {
-                val view: View = inputFieldsDate.getChildAt(i)
-                if (view is TextView) {
-                    dates.add(view.text.toString())
-                }
-            }
-            for (i in values){
-                Log.d("Text", i)
-            }
-            for (i in dates){
-                Log.d("Date", i)
-            }
+
         }
     }
-    private fun showDatePicker() {
-        // Create a DatePickerDialog
-        val datePickerDialog = DatePickerDialog(
-            this, {DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
-                val selectedDate = Calendar.getInstance()
-                selectedDate.set(year, monthOfYear, dayOfMonth)
-                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-                val formattedDate = dateFormat.format(selectedDate.time)
-                tvDatePicker.text = "$formattedDate"
-            },
-            calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH)
-        )
-        // Show the DatePicker dialog
-        datePickerDialog.show()
+    private fun showModal(){
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.popup_register_surg_history)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val btn_close:Button = dialog.findViewById(R.id.btn_modal_deny_surg)
+        val btn_confirm = dialog.findViewById<Button>(R.id.btn_modal_confirm_surg)
+        val input_med = dialog.findViewById<EditText>(R.id.edit_surg)
+        val input_date = dialog.findViewById<EditText>(R.id.edit_prev_surg_date)
+
+        input_date.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(
+                this, {DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                    val selectedDate = Calendar.getInstance()
+                    selectedDate.set(year, monthOfYear, dayOfMonth)
+                    val dateFormat = SimpleDateFormat("yyyy-dd-MM", Locale.getDefault())
+                    val formattedDate = dateFormat.format(selectedDate.time)
+                    input_date.setText("$formattedDate")
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            )
+            // Show the DatePicker dialog
+            datePickerDialog.show()
+        }
+        btn_confirm.setOnClickListener {
+            val inputFields = findViewById<LinearLayout>(R.id.inputFields3)
+            val inputFieldsDate = findViewById<LinearLayout>(R.id.inputFieldsDate3)
+            val inputField = TextView(this)
+            val inputFieldDate = TextView(this)
+            inputField.text = input_med.text
+            inputFieldDate.text = input_date.text
+
+            inputField.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                gravity = Gravity.CENTER
+            }
+            inputFieldDate.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                gravity = Gravity.CENTER
+            }
+            inputField.setPadding(41,25,25,25)
+            inputFieldDate.setPadding(41,25,25,25)
+            inputField.textSize = 18F
+            inputFieldDate.textSize = 18F
+
+
+            inputFields.addView(inputField)
+            inputFieldsDate.addView(inputFieldDate)
+            dialog.dismiss()
+        }
+
+        btn_close.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
+
 }
